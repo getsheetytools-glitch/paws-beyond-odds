27cafb272f2c7d52637dffea641036a001c500f4
nlajeu <nlajeu@gmail.com>
2026-03-28 19:02:29 -0400

    formatting

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // Set path prefix for GitHub Pages subpath deployment
+  // e.g. getsheetytools-glitch.github.io/paws-over-odds/
+  // Set ELEVENTY_PATH_PREFIX env var to override (use "/" for custom domain)
+  const pathPrefix = process.env.ELEVENTY_PATH_PREFIX || "/paws-over-odds/";
+
   // Add a readable date filter
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return new Date(dateObj).toLocaleDateString("en-CA", {
@@ -22,6 +27,7 @@ module.exports = function(eleventyConfig) {
       layouts: "_layouts",
       data: "_data"
     },
+    pathPrefix: pathPrefix,
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
     templateFormats: ["njk", "md", "html"]
